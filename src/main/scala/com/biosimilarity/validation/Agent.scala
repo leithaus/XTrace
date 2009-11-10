a0bb0cc85b8b3cb1318d24faa7622521c39bfc8e
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2009-11-10 15:37:52 -0800

    Added delimited continuation support

--- src/main/scala/com/biosimilarity/validation/Agent.scala
+++ src/main/scala/com/biosimilarity/validation/Agent.scala
@@ -11,11 +11,45 @@ package com.biosimilarity.validation
 import java.net.URI
 import com.eaio.uuid.UUID
 
+import scala.continuations._ 
+import scala.continuations.ControlContext._ 
 import scala.collection.mutable._
 import scala.actors._
 import Actor._
 
 import com.thoughtworks.xstream._
+
+trait Status[Msg] {
+  def message : Msg
+  def success : Boolean
+  def explanation : Option[String]
+  def continuation : Option[Status[Msg] => Status[Msg]]
+}
+
+abstract class HandlingStatus[Msg] (
+  message : Msg,
+  success : Boolean,
+  explanation : Option[String],
+  continuation : Option[Status[Msg] => Status[Msg]]
+) extends Status[Msg]
+
+case class JReqStatus(
+  message : JustifiedRequest,
+  success : Boolean,
+  explanation : Option[String],
+  continuation : Option[Status[JustifiedRequest] => Status[JustifiedRequest]]
+) extends HandlingStatus[JustifiedRequest](
+  message, success, explanation, continuation
+)
+
+case class JRspStatus(
+  message : JustifiedResponse,
+  success : Boolean,
+  explanation : Option[String],
+  continuation : Option[Status[JustifiedResponse] => Status[JustifiedResponse]]
+) extends HandlingStatus[JustifiedResponse](
+  message, success, explanation, continuation
+)
 
 trait Socialite {  
   def name : URI
@@ -167,6 +201,18 @@ trait Socialite {
   }
   def handle( response : JustifiedResponse ) : Boolean = {
     false
+  }
+  def handleWithContinuation(
+    request : JustifiedRequest,
+    k : Status[JustifiedRequest] => Status[JustifiedRequest]
+  ) : Status[JustifiedRequest] = {
+    JReqStatus( request, false, None, Some( k ) )
+  }
+  def handleWithContinuation(
+    response : JustifiedResponse,
+    k : Status[JustifiedResponse] => Status[JustifiedResponse]
+  ) : Status[JustifiedResponse] = {
+    JRspStatus( response, false, None, Some( k ) )
   }
   def handle( request : InspectRequests ) : Boolean = {
     println( "*** Requests ***" )
@@ -555,6 +601,82 @@ case class Messenger(
 	    if ( validate( jr ) ) {
 	      println( "calling handle on " + jr )
 	      handle( jr )
+	    }
+	    act()
+	  }
+	  case InspectRequests( t, f ) => {
+	    val ir = InspectRequests( t, f )
+	    if ( validate( ir ) ) {
+	      println( "calling handle on " + ir )
+	      handle( ir )
+	    }
+	    act()
+	  }
+	  case InspectResponses( t, f ) => {
+	    val ir = InspectResponses( t, f )
+	    if ( validate( ir ) ) {
+	      println( "calling handle on " + ir )
+	      handle( ir )
+	    }
+	    act()
+	  }
+	  case InspectNamespace( t, f ) => {
+	    val ir = InspectNamespace( t, f )
+	    if ( validate( ir ) ) {
+	      println( "calling handle on " + ir )
+	      handle( ir )
+	    }
+	    act()
+	  }
+	}
+      }
+    }    
+  }
+}
+
+case class RMessenger(
+  name : URI,
+  requests : ListBuffer[JustifiedRequest],
+  responses : ListBuffer[JustifiedResponse],
+  nameSpace : Option[LinkedHashMap[URI,Socialite]],
+  traceMonitor : TraceMonitor
+) extends Actor with Socialite {
+  override def useBraceNotation : Boolean = false
+  def likes( dsg : URI, acq : Socialite ) : Boolean = true
+  def act () {
+    nameSpace match {
+      case None => {
+	logError( name, this, NoNamespace() )
+      }
+      case Some( map ) => {
+	receive {
+	  case JustifiedRequest( m, p, d, t, f, c ) => {
+	    val jr = JustifiedRequest( m, p, d, t, f, c )
+	    if ( validate( jr ) ) {
+	      println( "calling handle on " + jr )
+	      reset {
+		shift {
+		  ( k : Status[JustifiedRequest] => Status[JustifiedRequest] )
+		  => {
+		    k( handleWithContinuation( jr, k ) )
+		  }
+		}
+	      }
+	    }
+	    act()
+	  }
+	  case JustifiedResponse( m, p, d, t, f, c ) =>  {
+	    val jr = JustifiedResponse( m, p, d, t, f, c )
+	    if ( validate( jr ) ) {
+	      println( "calling handle on " + jr )
+	      reset {
+		shift {
+		  ( k : Status[JustifiedResponse] => Status[JustifiedResponse] )
+		  => {
+		    k( handleWithContinuation( jr, k ) )
+		  }
+		}
+	      }
 	    }
 	    act()
 	  }
--- src/main/scala/com/biosimilarity/validation/Wizard.scala
+++ src/main/scala/com/biosimilarity/validation/Wizard.scala
@@ -11,6 +11,8 @@ package com.biosimilarity.validation
 import java.net.URI
 import com.eaio.uuid.UUID
 
+import scala.continuations._ 
+import scala.continuations.ControlContext._ 
 import scala.collection.mutable._
 import scala.actors._
 import Actor._
@@ -389,6 +391,377 @@ object Cabal {
 	 }
        }
 
+  case object RMessengerOne
+     extends RMessenger(
+       harrysName,
+       new ListBuffer[JustifiedRequest](),
+       new ListBuffer[JustifiedResponse](),
+       Some( new LinkedHashMap[URI,Socialite]() ),
+       ATraceMonitor
+       ) {
+	 val responsesReceived : ListBuffer[JustifiedResponse] =
+	   new ListBuffer[JustifiedResponse]()
+	 override def useBraceNotation : Boolean = true
+	 def script() = {	   
+	   markRequest( friendlyOverture )
+	   RMessengerThree ! friendlyOverture
+	   markRequest( expelliarmus )
+	   RMessengerFour ! expelliarmus
+	 }
+	 override def handleWithContinuation(
+	   response : JustifiedResponse,
+	   k : Status[JustifiedResponse] => Status[JustifiedResponse]
+	 ) = {
+	   //println( "handling: " + response )
+	   response match {
+	     case JustifiedResponse(
+	       mgc, content, Some( req ),
+	       _,
+	       respondersName,
+	       color
+	     ) => {
+	       //println( "matched response case!" )
+	       respondersName.getFragment match {
+		 case "Ron" => {
+		   //println( "matched Ron case!" )
+		   //throw new Exception( "shouldn't get here!" )
+		   k(
+		     JRspStatus(
+		       response,
+		       false,
+		       Some( "Identity fraud" ),
+		       Some( k )
+		     )
+		   )
+		 }
+		 case "Herminone" => {
+		   // traceMonitor ! CloseSession( RMessengerOne )
+		   // traceMonitor ! LogMessage( this, "Should close log" )
+		   //println( "matched Herminone case!" )
+		   //println( "closing monitor session" )		   
+		   // println(
+// 		     "session status : " + traceMonitor.sessionStatus( this )
+// 		   )
+		   responsesReceived.length match {
+		     case 1 => {
+		       responsesReceived += response
+		       traceMonitor.closeMonitoringSession( this )
+		     }
+		     case _ => {
+		       responsesReceived += response
+		       traceMonitor.traceEvent(
+			 this,
+			 "<waiting>Draco " + responses.length + "</waiting>"
+		       )
+		     }
+		   }
+		 }
+		 case "Draco" => {
+		   //println( "matched Draco case!" )
+		   //throw new Exception( "shouldn't get here!" )
+		   responsesReceived.length match {
+		     case 1 => {
+		       responsesReceived += response
+		       traceMonitor.closeMonitoringSession( this )
+		     }
+		     case _ => {
+		       responsesReceived += response
+		       // traceMonitor.traceEvent(
+// 			 this,
+// 			 "<waiting>Hermione " + responses.length + "</waiting>"
+// 		       )
+		     }
+		   }
+		 }
+		 case x => {
+		   //println( "unexpected name" + x )
+		   //throw new Exception( "shouldn't get here!" )
+		   k(
+		     JRspStatus(
+		       response,
+		       false,
+		       Some( "Identity fraud" ),
+		       Some( k )
+		     )
+		   )
+		 }
+	       }
+	     }
+	     case _ => {
+	       //println( "matched exception case!" )
+	       //throw new Exception( "That's no way to talk to a wizard!" )
+	       k(
+		 JRspStatus(
+		   response,
+		   false,
+		   Some( "Unexpected message" ),
+		   Some( k )
+		 )
+	       )
+	     }
+	   }
+	   JRspStatus(
+	     response,
+	     true,
+	     None,
+	     Some( k )
+	   )
+	 }
+       }
+
+  case object RMessengerTwo
+     extends RMessenger(
+       ronsName,
+       new ListBuffer[JustifiedRequest](),
+       new ListBuffer[JustifiedResponse](),
+       Some( new LinkedHashMap[URI,Socialite]() ),
+       ATraceMonitor
+       ) {
+	 override def useBraceNotation : Boolean = true
+	 def script() = {
+	   markRequest( pickUpLine )
+	   RMessengerThree ! pickUpLine
+	 }
+	 override def handleWithContinuation(
+	   response : JustifiedResponse,
+	   k : Status[JustifiedResponse] => Status[JustifiedResponse]
+	 ) = {
+	   //println( "handling: " + response )
+	   response match {
+	     case JustifiedResponse(
+	       mgc, content, Some( req ),
+	       _,
+	       respondersName,
+	       color
+	     ) => {
+	       respondersName.getFragment match {
+		 case "Harry" => {
+		   //throw new Exception( "shouldn't get here!" )
+		   k(
+		     JRspStatus(
+		       response,
+		       false,
+		       Some( "Identity fraud" ),
+		       Some( k )
+		     )
+		   )
+		 }
+		 case "Herminone" => {
+		   // traceMonitor ! CloseSession( RMessengerTwo )
+		   // traceMonitor ! LogMessage( this, "Should close log" )
+		   //println( "closing monitor session" )
+		   traceMonitor.closeMonitoringSession( this )
+		   // println(
+// 		     "session status : " + traceMonitor.sessionStatus( this )
+// 		   )
+		 }
+		 case "Draco" => {
+		   //throw new Exception( "shouldn't get here!" )
+		   k(
+		     JRspStatus(
+		       response,
+		       false,
+		       Some( "Identity fraud" ),
+		       Some( k )
+		     )
+		   )
+		 }
+	       }
+	     }
+	     case _ => {
+	       //throw new Exception( "That's no way to talk to a wizard!" )
+	       k(
+		 JRspStatus(
+		   response,
+		   false,
+		   Some( "Unexpected message" ),
+		   Some( k )
+		 )
+	       )
+	     }
+	   }
+	   JRspStatus(
+	     response,
+	     true,
+	     None,
+	     Some( k )
+	   )
+	 }
+       }
+
+  case object RMessengerThree
+     extends RMessenger(
+       hermionesName,
+       new ListBuffer[JustifiedRequest](),
+       new ListBuffer[JustifiedResponse](),
+       Some( new LinkedHashMap[URI,Socialite]() ),
+       ATraceMonitor
+       ) {
+	 override def useBraceNotation : Boolean = true
+	 def script() = {	   
+	   //markRequest( pickUpLine )
+	   //RMessengerThree ! pickUpLine
+	 }
+	 override def handleWithContinuation(
+	   request : JustifiedRequest,
+	   k : Status[JustifiedRequest] => Status[JustifiedRequest]
+	 ) = {
+	   //println( "handling: " + request )
+	   request match {
+	     case JustifiedRequest(
+	       0, content, None,
+	       _,
+	       suitorsName,
+	       color
+	     ) => {
+	       suitorsName.getFragment match {
+		 case "Ron" => {
+		   //println( "case Ron" )
+		   markResponse( flirt )
+		   RMessengerTwo ! flirt
+		   responses.length match {
+		     case 2 => {
+		       // traceMonitor ! CloseSession( RMessengerThree )
+		       // traceMonitor ! LogMessage( this, "Should close log" )	
+		       //println( "closing monitor session" )
+		       traceMonitor.closeMonitoringSession( this )
+		       // println(
+// 			 "session status : " + traceMonitor.sessionStatus( this )
+// 		       )
+		     }
+		     case _ => {
+		       traceMonitor.traceEvent(
+			 this,
+			 "<waiting>Harry " + responses.length + "</waiting>"
+		       )
+		     }
+		   }
+		 }
+		 case "Harry" => {
+		   //println( "case Harry" )
+		   markResponse( friendlyResponse )
+		   RMessengerOne ! friendlyResponse
+		   responses.length match {
+		     case 2 => {
+		       //traceMonitor ! CloseSession( RMessengerThree )
+		       //traceMonitor.traceEvent( this, "Should close log" )
+		       //println( "closing monitor session" )
+		       traceMonitor.closeMonitoringSession( this )
+		       // println(
+// 			 "session status : " + traceMonitor.sessionStatus( this )
+// 		       )
+		     }
+		     case _ => {
+		       traceMonitor.traceEvent(
+			 this,
+			 "<waiting>Ron " + responses.length + "</waiting>"
+		       )
+		     }
+		   }
+		 }
+	       }
+	     }
+	     case _ => {
+	       //println( "case got some other kind of message" )
+	       //throw new Exception( "That's no way to talk to a witch!" )
+	       k(
+		 JReqStatus(
+		   request,
+		   false,
+		   Some( "Unexpected message" ),
+		   Some( k )
+		 )
+	       )
+	     }
+	   }
+	   JReqStatus(
+	     request,
+	     true,
+	     None,
+	     Some( k )
+	   )
+	 }
+       }
+
+  case object RMessengerFour
+     extends RMessenger(
+       dracosName,
+       new ListBuffer[JustifiedRequest](),
+       new ListBuffer[JustifiedResponse](),
+       Some( new LinkedHashMap[URI,Socialite]() ),
+       ATraceMonitor
+       ) {
+	 override def useBraceNotation : Boolean = true
+	 def script() = {
+	   //markRequest( pickUpLine )
+	   //RMessengerThree ! pickUpLine
+	 }
+	 override def handleWithContinuation(
+	   request : JustifiedRequest,
+	   k : Status[JustifiedRequest] => Status[JustifiedRequest]
+	 ) = {
+	   println( "handling: " + request )
+	   request match {
+	     case JustifiedRequest(
+	       mgc, content, None,
+	       _,
+	       requestersName,
+	       color
+	     ) => {
+	       requestersName.getFragment match {
+		 case "Harry" => {
+		   //traceMonitor ! CloseSession( RMessengerFour )
+		   //traceMonitor.traceEvent( this, "Should close log" )
+		   println( "matched Harry case" )
+		   markResponse( finiteIncantatem )
+		   RMessengerOne ! finiteIncantatem
+		   traceMonitor.closeMonitoringSession( this )
+		   // println(
+// 		     "session status : " + traceMonitor.sessionStatus( this )
+// 		   )
+		 }
+		 case "Herminone" => {
+		   //traceMonitor ! CloseSession( RMessengerFour )
+		   //traceMonitor.traceEvent( this, "Should close log" )
+		   //println( "closing monitor session" )		   
+		   traceMonitor.closeMonitoringSession( this )
+		   // println(
+// 		     "session status : " + traceMonitor.sessionStatus( this )
+// 		   )
+		 }
+		 case "Ron" => {
+		   //traceMonitor ! CloseSession( RMessengerFour )
+		   //traceMonitor.traceEvent( this, "Should close log" )
+		   //println( "closing monitor session" )
+		   traceMonitor.closeMonitoringSession( this )
+		   // println(
+// 		     "session status : " + traceMonitor.sessionStatus( this )
+// 		   )
+		 }
+	       }
+	     }
+	     case _ => {
+	       //println( "Draco exception case" )
+	       //throw new Exception( "That's no way to talk to a wizard!" )
+	       k(
+		 JReqStatus(
+		   request,
+		   false,
+		   Some( "Unexpected message" ),
+		   Some( k )
+		 )
+	       )
+	     }
+	   }
+	   JReqStatus(
+	     request,
+	     true,
+	     None,
+	     Some( k )
+	   )
+	 }
+       }
+
   def hookup() = {
     // MessengerOne learns his name is Harry
     MessengerOne.introduce( harrysName, MessengerOne )
@@ -433,6 +806,52 @@ object Cabal {
     MessengerFour.introduce( hermionesName, MessengerThree )
     // Connect to log
     ATraceMonitor.openMonitoringSession( MessengerFour )
+
+    // This community reuses the names for different identities
+
+    // RMessengerOne learns his name is Harry
+    RMessengerOne.introduce( harrysName, RMessengerOne )
+    // Harry is introduced to RMessengerTwo known as Ron
+    RMessengerOne.introduce( ronsName, RMessengerTwo )
+    // Harry is introduced to RMessengerThree known as Herminone
+    RMessengerOne.introduce( hermionesName, RMessengerThree )
+    // Harry is introduced to RMessengerFour known as Draco
+    RMessengerOne.introduce( dracosName, RMessengerFour )
+    // Connect to log
+    ATraceMonitor.openMonitoringSession( RMessengerOne )
+
+    // RMessengerTwo learns his name is Ron
+    RMessengerTwo.introduce( ronsName, RMessengerTwo )
+    // Ron is introduced to RMessengerOne known as Harry
+    RMessengerTwo.introduce( harrysName, RMessengerOne )
+    // Ron is introduced to RMessengerThree known as Herminone
+    RMessengerTwo.introduce( hermionesName, RMessengerThree )
+    // Ron is introduced to RMessengerFour known as Draco
+    RMessengerTwo.introduce( dracosName, RMessengerFour )
+    // Connect to log
+    ATraceMonitor.openMonitoringSession( RMessengerTwo )
+
+    // RMessengerThree learns her name is Hermione
+    RMessengerThree.introduce( hermionesName, RMessengerThree )
+    // Hermione is introduced to RMessengerOne known as Harry
+    RMessengerThree.introduce( harrysName, RMessengerOne )
+    // Hermione is introduced to RMessengerTwo known as Ron
+    RMessengerThree.introduce( ronsName, RMessengerTwo )
+    // Hermione is introduced to RMessengerFour known as Draco
+    RMessengerThree.introduce( dracosName, RMessengerFour )
+    // Connect to log
+    ATraceMonitor.openMonitoringSession( RMessengerThree )
+
+    // RMessengerFour learns his name is Draco
+    RMessengerFour.introduce( dracosName, RMessengerFour )
+    // Draco is introduced to RMessengerOne known as Harry
+    RMessengerFour.introduce( harrysName, RMessengerOne )
+    // Draco is introduced to RMessengerTwo known as Ron
+    RMessengerFour.introduce( ronsName, RMessengerTwo )
+    // Draco is introduced to RMessengerThree known as Hermione
+    RMessengerFour.introduce( hermionesName, RMessengerThree )
+    // Connect to log
+    ATraceMonitor.openMonitoringSession( RMessengerFour )
   }
 
   def activate() = {
@@ -440,6 +859,12 @@ object Cabal {
     MessengerTwo.start
     MessengerThree.start
     MessengerFour.start
+
+    RMessengerOne.start
+    RMessengerTwo.start
+    RMessengerThree.start
+    RMessengerFour.start
+
     ATraceMonitor.start
   }
 
@@ -448,6 +873,11 @@ object Cabal {
     MessengerTwo.script()
     MessengerThree.script()
     MessengerFour.script()
+
+    RMessengerOne.script()
+    RMessengerTwo.script()
+    RMessengerThree.script()
+    RMessengerFour.script()
     // traceMonitor ! ShowLog()
   }
 }
